bbd6b9c7a05ddc54b07ccb4605da28713dcedea1
Thom Wright <github@thomwright.co.uk>
2017-10-10 21:31:51 +0100

    Expose visibilities from public package-level API

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ const createPublicApi = require("./container")
 
 module.exports = createPublicApi()
 module.exports.lifetimes = require("./lifetimes")
+module.exports.visibilities = require("./visibility").visibilities
